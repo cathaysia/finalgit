751eb16d6c28c544d6ddaee8f56f015b41f63ecb
loongtao.zhang <loongtao.zhang@outlook.com>
2024-09-06 09:35:58 +0800

    feat: add devtools
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -2,16 +2,19 @@ import { create } from "zustand";
 import { match } from "ts-pattern";
 import { BranchInfo, TagInfo, commands } from "@/bindings";
 import { useErrorState } from "./error";
+import { devtools } from "zustand/middleware";
 
 export interface OpenState {
 	isOpened: boolean;
 	setIsOpened: (isOpened: boolean) => void;
 }
 
-export const useOpenState = create<OpenState>((set) => ({
-	isOpened: false,
-	setIsOpened: (isOpened: boolean) => set({ isOpened: isOpened }),
-}));
+export const useOpenState = create<OpenState>()(
+	devtools((set) => ({
+		isOpened: false,
+		setIsOpened: (isOpened: boolean) => set({ isOpened: isOpened }),
+	})),
+);
 
 export interface BranchState {
 	branches: BranchInfo[];
@@ -19,21 +22,23 @@ export interface BranchState {
 	refreshBranches: () => void;
 }
 
-export const useBranchState = create<BranchState>((set) => ({
-	branches: [],
-	setBranches: (branches: BranchInfo[]) => set({ branches: branches }),
-	refreshBranches: () => {
-		commands.getBranchInfo().then((value) => {
-			match(value)
-				.with({ status: "ok" }, (v) => {
-					set({ branches: v.data });
-				})
-				.with({ status: "error" }, (err) => {
-					console.log(err.error);
-				});
-		});
-	},
-}));
+export const useBranchState = create<BranchState>()(
+	devtools((set) => ({
+		branches: [],
+		setBranches: (branches: BranchInfo[]) => set({ branches: branches }),
+		refreshBranches: () => {
+			commands.getBranchInfo().then((value) => {
+				match(value)
+					.with({ status: "ok" }, (v) => {
+						set({ branches: v.data });
+					})
+					.with({ status: "error" }, (err) => {
+						console.log(err.error);
+					});
+			});
+		},
+	})),
+);
 
 export interface TagState {
 	tags: TagInfo[];
@@ -41,28 +46,32 @@ export interface TagState {
 	refreshTags: () => void;
 }
 
-export const useTagStatte = create<TagState>((set) => ({
-	tags: [],
-	setTags: (tags: TagInfo[]) => set({ tags: tags }),
-	refreshTags: () => {
-		commands.getTagInfo().then((value) => {
-			match(value)
-				.with({ status: "ok" }, (v) => {
-					set({ tags: v.data });
-				})
-				.with({ status: "error" }, (err) => {
-					console.log(err.error);
-				});
-		});
-	},
-}));
+export const useTagStatte = create<TagState>()(
+	devtools((set) => ({
+		tags: [],
+		setTags: (tags: TagInfo[]) => set({ tags: tags }),
+		refreshTags: () => {
+			commands.getTagInfo().then((value) => {
+				match(value)
+					.with({ status: "ok" }, (v) => {
+						set({ tags: v.data });
+					})
+					.with({ status: "error" }, (err) => {
+						console.log(err.error);
+					});
+			});
+		},
+	})),
+);
 
 export interface CommitState {
 	commit: string | null;
 	setCommit: (commit: string | null) => void;
 }
 
-export const useCommitState = create<CommitState>((set) => ({
-	commit: null,
-	setCommit: (commit: string | null) => set({ commit: commit }),
-}));
+export const useCommitState = create<CommitState>()(
+	devtools((set) => ({
+		commit: null,
+		setCommit: (commit: string | null) => set({ commit: commit }),
+	})),
+);

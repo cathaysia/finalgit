a54855476ecc1cbddf96260e4c716be1fa3c0bdd
loongtao.zhang <loongtao.zhang@outlook.com>
2024-09-08 22:44:51 +0800

    feat: add storybook
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

---
+++ src/stories/Nav.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Nav } from "./Nav";
+
+const meta = {
+	component: Nav,
+} satisfies Meta<typeof Nav>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		to: "to",
+		text: {},
+	},
+};

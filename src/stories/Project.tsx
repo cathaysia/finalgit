5a97aa93d8b34318e80e4ee66205902b38fb2e9c
loongtao.zhang <loongtao.zhang@outlook.com>
2024-09-11 11:24:41 +0800

    feat(stories): add project
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

---
+++ src/stories/Project.tsx
@@ -0,0 +1,67 @@
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import { useTranslation } from "react-i18next";
+import { FaCheck } from "react-icons/fa";
+import { IoIosAdd } from "react-icons/io";
+import { MdAddToPhotos } from "react-icons/md";
+import { BsChevronExpand } from "react-icons/bs";
+
+export interface ProjectProps {
+	current: string;
+	projects?: string[];
+	className?: string;
+}
+
+export default function Project({
+	current,
+	projects = [],
+	className,
+}: ProjectProps) {
+	const { t, i18n } = useTranslation();
+	return (
+		<div className={cn("w-full", className)}>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button className="w-52 flex justify-between">
+						<span>{current}</span>
+						<BsChevronExpand />
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className="w-52">
+					<DropdownMenuGroup>
+						{projects.map((item) => {
+							return (
+								<DropdownMenuItem
+									className={cn(
+										"flex justify-between",
+										item == current && "bg-slate-200",
+									)}
+								>
+									<span>{item}</span>
+									{item == current && <FaCheck />}
+								</DropdownMenuItem>
+							);
+						})}
+						{projects.length != 0 && <DropdownMenuSeparator />}
+						<DropdownMenuItem className="flex justify-between">
+							<span>{t("Add local repository")}</span>
+							<IoIosAdd />
+						</DropdownMenuItem>
+						<DropdownMenuItem className="flex justify-between">
+							<span>{t("Clone repository")}</span>
+							<MdAddToPhotos />
+						</DropdownMenuItem>
+					</DropdownMenuGroup>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</div>
+	);
+}

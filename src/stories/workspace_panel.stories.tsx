979b010bd4ccbf0f4abfc306b8034949d6f9238f
loongtao.zhang <loongtao.zhang@outlook.com>
2024-09-11 14:06:13 +0800

    feat: add workspace_panel
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

---
+++ src/stories/workspace_panel.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import WorkspacePanel from "./workspace_panel";
+
+const meta = {
+	component: WorkspacePanel,
+} satisfies Meta<typeof WorkspacePanel>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		branchName: "master",
+		upstream: "origin/master",
+		changeSet: [],
+	},
+};

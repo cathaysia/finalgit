fa82619a02ed52400bd1731e0d07ef06c669491c
loongtao.zhang <loongtao.zhang@outlook.com>
2024-09-12 10:13:00 +0800

    feat(stories): add more operator
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- src/stories/Commiter.tsx
+++ src/stories/Commiter.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FaMagic } from "react-icons/fa";
+import { VscDiff } from "react-icons/vsc";
+import { VscGitStash } from "react-icons/vsc";
+
+import { VscDiscard } from "react-icons/vsc";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export interface CommiterProps
     extends React.HtmlHTMLAttributes<HTMLDivElement> {}
@@ -14,10 +26,33 @@ export default function Commiter({ className, ...props }: CommiterProps) {
 
     if (!isCommiting) {
         return (
-            <div className={cn(className)}>
+            <div className={cn("flex gap-2", className)}>
                 <Button className="w-full" onClick={() => setIsCommiting(true)}>
                     {t("commiter.start_commit")}
                 </Button>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant={"ghost"}>
+                            <DotsHorizontalIcon />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <VscDiff className="w-4 h-4 mr-2" />
+                                {t("workspace.generate_patch")}
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <VscGitStash className="w-4 h-4 mr-2" />
+                                {t("workspace.stash")}
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className="text-red-600">
+                                <VscDiscard className="w-4 h-4 mr-2" />
+                                {t("workspace.discard")}
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         );
     }

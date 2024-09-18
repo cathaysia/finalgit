ce075035dc1a8ecd5d6868fe94a9f3d6233b0ab8
loongtao.zhang <loongtao.zhang@outlook.com>
2024-09-18 21:40:02 +0800

    feat(route): only show route devtool in dev
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,25 @@
 import MainPanel from "@/stories/panels/MainPanel";
-import { createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { useQuery } from "@tanstack/react-query";
 import { commands } from "@/bindings";
 import { useAppState, useRefreshRequest } from "@/lib/state";
 import { match } from "ts-pattern";
 import { useErrorState } from "@/lib/error";
 import { trace } from "@tauri-apps/plugin-log";
+import React from "react";
 
 export const Route = createRootRoute({
     component: rootRoute,
 });
+
+const TanStackRouterDevtools =
+    process.env.NODE_ENV === "production"
+        ? () => null
+        : React.lazy(() =>
+              import("@tanstack/router-devtools").then((res) => ({
+                  default: res.TanStackRouterDevtools,
+              })),
+          );
 
 function rootRoute() {
     const repo_path = useAppState((s) => s.repo_path);
@@ -50,9 +59,7 @@ function rootRoute() {
         <>
             <MainPanel project_name={""} branches={[]} tags={[]} />
             <hr />
-            {
-                // <Outlet />
-            }
+            <Outlet />
             <TanStackRouterDevtools />
         </>
     );

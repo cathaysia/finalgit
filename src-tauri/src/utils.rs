67d10d5e1ec6a74346649d5911dbc7e15168dd83
loongtao.zhang <loongtao.zhang@outlook.com>
2024-09-18 09:41:39 +0800

    fix(filetime): fix filetime problem
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,6 +1,5 @@
-use std::os::linux::fs::MetadataExt;
-
 use crate::AppResult;
+use filetime::FileTime;
 
 pub fn open_repo(repo_path: &str) -> AppResult<git2::Repository> {
     Ok(git2::Repository::open(repo_path)?)
@@ -9,14 +8,14 @@ pub fn open_repo(repo_path: &str) -> AppResult<git2::Repository> {
 #[tauri::command]
 #[specta::specta]
 pub fn get_head_modify_time(repo_path: &str) -> AppResult<f64> {
-    let path = std::path::Path::new(repo_path);
-    let path = path.join(".git").canonicalize()?;
+    let path = std::path::Path::new(repo_path).canonicalize()?;
 
     if !path.exists() || !path.is_dir() {
         return Err(crate::AppError::NoRepo);
     }
 
     let meta = path.metadata()?;
+    let mtime = FileTime::from_last_modification_time(&meta);
 
-    Ok(meta.st_mtime() as f64)
+    Ok(mtime.seconds() as f64)
 }

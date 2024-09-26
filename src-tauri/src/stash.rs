83983974446ab1c9474609ec8ddb069390777a98
loongtao.zhang <loongtao.zhang@outlook.com>
2024-09-26 18:01:14 +0800

    feat(stash): allow list stash
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- src-tauri/src/stash.rs
+++ src-tauri/src/stash.rs
@@ -2,9 +2,17 @@ use crate::ext::RepoExt;
 
 use crate::AppResult;
 
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub struct StashInfo {
+    id: usize,
+    message: String,
+    oid: String,
+}
+
 pub trait StashExt {
     fn stash(&self) -> AppResult<()>;
     fn stash_pop(&self) -> AppResult<()>;
+    fn stash_list(&mut self) -> AppResult<Vec<StashInfo>>;
 }
 
 impl StashExt for git2::Repository {
@@ -16,5 +24,19 @@ impl StashExt for git2::Repository {
     fn stash_pop(&self) -> AppResult<()> {
         let _ = self.exec_git(["stash", "pop"])?;
         Ok(())
+    }
+
+    fn stash_list(&mut self) -> AppResult<Vec<StashInfo>> {
+        let mut infos = vec![];
+        self.stash_foreach(|idx, message, id| {
+            infos.push(StashInfo {
+                id: idx,
+                message: message.into(),
+                oid: id.to_string(),
+            });
+            true
+        })?;
+
+        Ok(infos)
     }
 }

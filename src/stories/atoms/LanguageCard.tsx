bf2b198c4428606c83b4699e131fb989fe6bb7ab
loongtao.zhang <loongtao.zhang@outlook.com>
2024-10-06 20:01:23 +0800

    feat(app): add language card
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

---
+++ src/stories/atoms/LanguageCard.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import i18n from '@/locales';
+import { Check, ChevronsUpDown } from 'lucide-react';
+import { useTranslation } from 'react-i18next';
+
+import { Button } from '@/components/ui/button';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+
+export function LanguageCard() {
+  const lang = i18n.language;
+  const languages = [
+    { label: 'English', value: 'en_US' },
+    { label: 'Chinese', value: 'zh_CN' },
+  ];
+  const { t } = useTranslation();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          className={cn(
+            'w-[200px] justify-between',
+            !lang && 'text-muted-foreground',
+          )}
+        >
+          {lang
+            ? languages.find(language => language.value === lang)?.label
+            : 'Select language'}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder={t('settings.search_langing')} />
+          <CommandList>
+            <CommandEmpty>No language found.</CommandEmpty>
+            <CommandGroup>
+              {languages.map(language => (
+                <CommandItem
+                  value={language.label}
+                  key={language.value}
+                  onSelect={() => {
+                    i18n.changeLanguage(language.value);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      'mr-2 h-4 w-4',
+                      language.value === lang ? 'opacity-100' : 'opacity-0',
+                    )}
+                  />
+                  {language.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- src/stories/settings/ThemeCard.tsx
+++ src/stories/settings/ThemeCard.tsx
@@ -1,31 +1,41 @@
 'use client';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
 import { cn } from '@/lib/utils';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
+import { useTranslation } from 'react-i18next';
 import { MdSettings } from 'react-icons/md';
+import { LanguageCard } from '../atoms/LanguageCard';
 
 export interface ThemeCardProps extends React.ComponentProps<typeof Card> {}
 
 export default function ThemeCard({ className, ...props }: ThemeCardProps) {
   const { setTheme } = useTheme();
+  const { t } = useTranslation();
 
   return (
     <Card className={cn(className)} {...props}>
       <CardHeader>
         <CardTitle>Theme</CardTitle>
       </CardHeader>
-      <CardContent className="flex w-full items-center justify-between">
-        <Button className="w-1/4" onClick={() => setTheme('light')}>
-          <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
-        <Button className="w-1/4" onClick={() => setTheme('dark')}>
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
-        <Button className="w-1/4" onClick={() => setTheme('system')}>
-          <MdSettings className="absolute h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0 dark:scale-100" />
-        </Button>
+      <CardContent className="flex w-full flex-col gap-2">
+        <div className="flex w-full justify-between">
+          <Button className="w-1/4" onClick={() => setTheme('light')}>
+            <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0 dark:scale-100" />
+          </Button>
+          <Button className="w-1/4" onClick={() => setTheme('dark')}>
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0 dark:scale-100" />
+          </Button>
+          <Button className="w-1/4" onClick={() => setTheme('system')}>
+            <MdSettings className="absolute h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0 dark:scale-100" />
+          </Button>
+        </div>
+        <div className="flex w-full items-center justify-between">
+          <Label>{t('profile.language')}</Label>
+          <LanguageCard />
+        </div>
       </CardContent>
     </Card>
   );

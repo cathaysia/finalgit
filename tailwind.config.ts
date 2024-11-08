e104b4228a29b52d7e2d0d69ccc533d79afd2091
loongtao.zhang <loongtao.zhang@outlook.com>
2024-11-08 12:13:02 +0800

    refactor: replace tailwindcss-animate with tailwindcss-motion
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,7 +86,7 @@ const config: Config = {
     },
   },
   plugins: [
-    require('tailwindcss-animate'),
+    require('tailwindcss-motion'),
     require('@tailwindcss/container-queries'),
   ],
 };

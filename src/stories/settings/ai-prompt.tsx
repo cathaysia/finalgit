cb09f3d088d9bd836499da7fef4b9a25646542ef
loongtao.zhang <loongtao.zhang@outlook.com>
2024-11-14 13:33:35 +0800

    feat: Add link to conventional and gitmoji documentation in AI prompt settings

--- src/stories/settings/ai-prompt.tsx
+++ src/stories/settings/ai-prompt.tsx
@@ -8,9 +8,12 @@ import {
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { useAiState } from '@/hooks/state';
+import { cn } from '@/lib/utils';
+import { open } from '@tauri-apps/plugin-shell';
 import { useEffect, useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
+import { FaLink } from 'react-icons/fa';
 
 export default function AiPrompt() {
   const { t } = useTranslation();
@@ -37,33 +40,52 @@ export default function AiPrompt() {
     setPrompt(current, content);
   }, [content]);
 
+  const isConventional = current === 'Conventional Commits';
+  const isGitmoji = current === 'GitMoji';
+
   return (
     <Card className="w-full">
       <CardHeader>
         <CardTitle>{t('ai.prompt')}</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
-        <Select
-          defaultValue={current}
-          onValueChange={val => {
-            setCurrent(val);
-          }}
-        >
-          <SelectTrigger>
-            <SelectValue defaultValue={current} />
-          </SelectTrigger>
-          <SelectContent>
-            {list.map(name => {
-              return (
-                <SelectItem value={name} key={name}>
-                  {name}
-                </SelectItem>
-              );
-            })}
-          </SelectContent>
-        </Select>
+        <div className="flex items-center gap-2">
+          <Select
+            defaultValue={current}
+            onValueChange={val => {
+              setCurrent(val);
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue defaultValue={current} />
+            </SelectTrigger>
+            <SelectContent>
+              {list.map(name => {
+                return (
+                  <SelectItem value={name} key={name}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectContent>
+          </Select>
+          <FaLink
+            className={cn(
+              'hover:text-foreground/50',
+              !isConventional && !isGitmoji && 'hidden',
+            )}
+            onClick={() => {
+              if (isConventional) {
+                open('https://www.conventionalcommits.org/');
+              }
+              if (isGitmoji) {
+                open('https://gitmoji.dev/');
+              }
+            }}
+          />
+        </div>
         <Textarea
-          className="h-52 text-base"
+          className="h-80 resize-none text-base"
           onChange={val => {
             setContent(val.target.value);
           }}

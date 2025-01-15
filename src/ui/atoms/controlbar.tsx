2a970b427f1126c57fe123cf2c1987e025ea8eee
loongtao.zhang <loongtao.zhang@outlook.com>
2025-01-15 23:21:45 +0800

    feat: use native controls
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- src/ui/atoms/controlbar.tsx
+++ src/ui/atoms/controlbar.tsx
@@ -1,28 +1,13 @@
-'use client';
 import { Link } from '@/i18n/routing';
 import { cn } from '@/lib/utils';
-import { getCurrentWindow } from '@tauri-apps/api/window';
 import { motion } from 'motion/react';
 import { useTranslations } from 'next-intl';
-import { useEffect, useState } from 'react';
-import { IoClose, IoRemove } from 'react-icons/io5';
 import { MdSettings } from 'react-icons/md';
-import { RiExpandLeftRightFill } from 'react-icons/ri';
-import { RiContractLeftRightFill } from 'react-icons/ri';
 
 type ControlBarProps = React.HtmlHTMLAttributes<HTMLDivElement>;
 
 export default function ControlBar({ className, ...props }: ControlBarProps) {
   const t = useTranslations();
-  const [isMaximized, setIsMaximized] = useState(false);
-
-  useEffect(() => {
-    getCurrentWindow()
-      .isMaximizable()
-      .then(item => {
-        setIsMaximized(!item);
-      });
-  });
 
   return (
     <div
@@ -30,37 +15,7 @@ export default function ControlBar({ className, ...props }: ControlBarProps) {
       className={cn('flex h-8 w-full items-center justify-between', className)}
       {...props}
     >
-      <div className="flex items-center gap-2">
-        <div
-          className="group h-3 w-3 rounded-lg bg-red-600"
-          onClick={() => {
-            getCurrentWindow().close();
-          }}
-        >
-          <IoClose className="hidden h-3 w-3 text-gray-800 group-hover:block" />
-        </div>
-        <div
-          className="group h-3 w-3 rounded-lg bg-yellow-600"
-          onClick={() => {
-            getCurrentWindow().minimize();
-          }}
-        >
-          <IoRemove className="hidden h-3 w-3 text-gray-800 group-hover:block" />
-        </div>
-        <div
-          className="group h-3 w-3 rounded-lg bg-green-600"
-          onClick={() => {
-            getCurrentWindow().toggleMaximize();
-            setIsMaximized(!isMaximized);
-          }}
-        >
-          {isMaximized ? (
-            <RiContractLeftRightFill className="hidden h-3 w-3 rotate-45 text-gray-800 group-hover:block" />
-          ) : (
-            <RiExpandLeftRightFill className="hidden h-3 w-3 rotate-45 text-gray-800 group-hover:block" />
-          )}
-        </div>
-      </div>
+      <div className="flex items-center gap-2" />
       <motion.span
         className="hover:text-foreground/80"
         whileHover={{ rotate: '90deg', transitionDuration: '150ms' }}

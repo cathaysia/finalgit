3b9a66c5b5c42569bdba6416c07ef1289fa2297a
loongtao.zhang <loongtao.zhang@outlook.com>
2025-02-13 10:39:57 +0800

    fix(lang): fix lang load problem
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,44 @@
 'use client';
+import '@/assets/global.css';
+
+import { Skeleton } from '@/components/ui/skeleton';
 import { useAppStore } from '@/hooks/use-store';
 import { redirect } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import { ThemeProvider } from 'next-themes';
 
 export default function Page() {
-  const lang = useAppStore(s => s.lang);
-  if (lang === 'cn') {
-    redirect('/cn');
-  }
+  const [lang, setLang] = useState<string | null>(null);
 
-  redirect('/en');
-  return <></>;
+  const [isHydrated, storeLang] = useAppStore(s => [s.isHydrated, s.lang]);
+
+  useEffect(() => {
+    if (isHydrated) {
+      setLang(storeLang);
+    }
+  }, [isHydrated]);
+
+  if (lang) {
+    redirect(`/${lang}`);
+  }
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="h-screen w-screen">
+        <ThemeProvider
+          defaultTheme="system"
+          storageKey="next-themes"
+          enableSystem
+          attribute="class"
+        >
+          <div className="grid h-full w-full grid-cols-4 gap-2 p-4">
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+            <Skeleton />
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }

d6c7dbcab348c9a041d2853a71b8d5306f4f5874
loongtao.zhang <loongtao.zhang@outlook.com>
2025-03-09 10:46:52 +0800

    feat: remove custom controlbar for linux and windows
    Signed-off-by: loongtao.zhang <loongtao.zhang@outlook.com>

--- src/app/[locale]/main/ui/controlbar.tsx
+++ src/app/[locale]/main/ui/controlbar.tsx
@@ -3,28 +3,9 @@ import { Link } from '@/i18n/routing';
 import { cn } from '@/lib/utils';
 import { motion } from 'motion/react';
 import { useTranslations } from 'next-intl';
-import React from 'react';
 import { MdSettings } from 'react-icons/md';
 
 type ControlBarProps = React.HtmlHTMLAttributes<HTMLDivElement>;
-
-const LinuxControl =
-  process.env.os === 'linux'
-    ? React.lazy(() =>
-        import('@/app/[locale]/main/ui/linux-control').then(res => ({
-          default: res.LinuxControl,
-        })),
-      )
-    : () => null;
-
-const WindowsControl =
-  process.env.os === 'win32'
-    ? React.lazy(() =>
-        import('@/app/[locale]/main/ui/windows-control').then(res => ({
-          default: res.Win32Control,
-        })),
-      )
-    : () => null;
 
 export default function ControlBar({ className, ...props }: ControlBarProps) {
   const t = useTranslations();
@@ -35,10 +16,7 @@ export default function ControlBar({ className, ...props }: ControlBarProps) {
       className={cn('flex h-8 w-full items-center justify-between', className)}
       {...props}
     >
-      <div className="flex items-center gap-2">
-        <LinuxControl />
-        <WindowsControl />
-      </div>
+      <div className="flex items-center gap-2" />
       <motion.span
         className="hover:text-foreground/80"
         whileHover={{ rotate: '90deg', transitionDuration: '150ms' }}
